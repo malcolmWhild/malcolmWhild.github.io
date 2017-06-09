b49da84d87c5633e946de35292eab49e95280ddf
malcolmWhild <malcolm.whild@gmail.com>
2017-06-10 08:09:51 +1200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,30 @@
 // JavaScript Document
-$(document).ready(function(){
+$(document).ready(function () {
 	"use strict";
-	$("#rest").hide();
-	$(".returnToKeySkills").hide();
-	$("#dataProcessingItem").click(function(){
+	$("#dataProcessing").hide();
+	$("#vdpDocumentSetup").hide();
+	$("#edmCampaigns").hide();
+	$("#multichannelCampaigns").hide();
+	$("#dataProcessingLink").click(function () {
 		$("#dataProcessing").slideDown(200);
-		$("#keySkills").slideUp(200);
+		$("#mainIntro").slideUp(200);
+	$("#vdpDocumentSetup").hide();
+	$("#edmCampaigns").hide();
+	$("#multichannelCampaigns").hide();
 	});
-	$(".returnToKeySkills").click(function(){
-		$("#keySkills").slideDown(200);
+	$("#vdpDocumentSetupLink").click(function () {
+		$("#vdpDocumentSetup").slideDown(200);
+		$("#mainIntro").slideUp(200);
 		$("#dataProcessing").hide();
-		$("#rest").hide();
-		$(".returnToKeySkills").hide();
+		$("#edmCampaigns").hide();
+		$("#multichannelCampaigns").hide();
+	});
+	$(".toTop").click(function () {
+		$("#mainIntro").slideDown(200);
+		$(".navbar-collapse").hide();
+		$("#dataProcessing").hide();
+		$("#vdpDocumentSetup").hide();
+		$("#edmCampaigns").hide();
+		$("#multichannelCampaigns").hide();
 	});
 });
